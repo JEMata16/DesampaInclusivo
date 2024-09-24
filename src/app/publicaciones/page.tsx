a07818ca8725ddca7bad5797b412b2d236304052
Kike <josellique@gmail.com>
2024-09-24 13:43:33 -0600

    Add: Minio

---
+++ src/app/publicaciones/page.tsx
@@ -0,0 +1,9 @@
+export default function publicaciones() {
+
+    return(
+        <main className="flex min-h-screen flex-col items-center justify-center bg-gray-300">
+            Publicaciones
+        </main>
+        
+    )
+}
